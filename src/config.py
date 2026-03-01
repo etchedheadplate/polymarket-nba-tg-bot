1c2e4d09f0b0f3629011ab0667659e23e03cfd9a
etchedheadplate <94640304+etchedheadplate@users.noreply.github.com>
2026-03-01 12:16:32 +0300

    Updatetd env vars

--- src/config.py
+++ src/config.py
@@ -1,8 +1,23 @@
+from pathlib import Path
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    SERVICE_NAME: str
+    LOG_DIR: Path
+
     TG_BOT_TOKEN: str
+
+    RABBITMQ_HOST: str
+    RABBITMQ_PORT: int
+    RABBITMQ_USER: str
+    RABBITMQ_PASSWORD: str
+    RABBITMQ_VHOST: str
+
+    EXCHANGE_NAME: str
+    RK_ORACLE_QUERY: str
+    RK_ORACLE_RESPONSE: str
 
     model_config = SettingsConfigDict(env_file=".env")
 
