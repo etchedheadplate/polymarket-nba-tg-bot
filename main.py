edec69cdaf002f4e00121daa3b9c2c1a713b910b
etchedheadplate <94640304+etchedheadplate@users.noreply.github.com>
2026-03-01 12:18:09 +0300

    Updated entry point

--- main.py
+++ main.py
@@ -1,24 +1,6 @@
 import asyncio
 
-from aiogram import Bot, Dispatcher
-from aiogram.filters import Command
-from aiogram.types import Message
-
-from src.config import settings
-
-TOKEN = settings.TG_BOT_TOKEN
-
-
-async def main():
-    bot = Bot(token=TOKEN)
-    dp = Dispatcher()
-
-    @dp.message(Command("start"))
-    async def start_handler(message: Message):  # pyright: ignore[reportUnusedFunction]
-        await message.answer("Hello!")
-
-    await dp.start_polling(bot)  # pyright: ignore[reportUnknownMemberType]
-
+from src.bot.bot import main
 
 if __name__ == "__main__":
     asyncio.run(main())
